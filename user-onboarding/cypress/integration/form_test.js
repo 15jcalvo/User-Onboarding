076e96d19132dffb1341c315d84219806ca2a528
James_Calvo <15jcalvo@gmail.com>
2021-11-11 14:49:35 -0500

    Form validation tests written, all tests working properly

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -14,26 +14,36 @@ describe('User onboarding app', () => {
     const termsInput = () => cy.get('input[name=terms]');
     const submitBtn = () => cy.get('button[id="submitBtn"')
 
-    it('Name input functionality', () => {
+    it('Name input test', () => {
         nameInput().type('James');
         nameInput().should('be.value', 'James');
     })
-    it('Email input functionality', () => {
+    it('Email input test', () => {
         emailInput().type('james@james.com');
         emailInput().should('be.value','james@james.com');
     })
-    it('Password input functionality', () => {
+    it('Password input test', () => {
         passwordInput().type('calvo');
         passwordInput().should('be.value','calvo');
     })
-    it('Terms checkboxes functionality', () => {
+    it('Terms checkboxes test', () => {
         termsInput().click({multiple: true});
     })
-    it('Submit button functionality', () => {
+    it('Submit button test', () => {
         nameInput().type('James');
         emailInput().type('james@james.com');
         passwordInput().type('calvo');
         termsInput().first().click();
         submitBtn().click();
+    })
+    it('Form validation test', () => {
+        nameInput().type('a');
+        cy.contains('Username must be 3 characters').should('exist');
+        emailInput().type('a');
+        cy.contains('Must be a valid email address').should('exist');
+        passwordInput().type('a');
+        cy.contains('Password must be 3 characters').should('exist');
+        termsInput().last().click();
+        cy.contains('Must accept terms').should('exist');
     })
 })
